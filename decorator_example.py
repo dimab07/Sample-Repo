b39436942b65fd329bd55d1c9e4d35e88f90c25d
dmitry.bron@gmail.com <dmitry@precision.dim.fox>
2020-11-21 23:21:02 +0200

    Decorator example

---
+++ decorator_example.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+
+def wrap(func):
+    def wrapped_func(*args):
+        print("Enter")
+        result = func(*args)
+        print(result)
+        print("Exit")
+        return result
+
+    return wrapped_func
+
+
+@wrap
+def add(a, b):
+    return a + b
+
+
+@wrap
+def mull(a, b):
+    return a * b
+
+
+# add = wrap(add) - The equal to @wrap ; def add(a, b):
+# mul = wrap(mul) - The equal to @wrap ; def mul(a, b):
+
+a, b = 3, 5
+
+print(add(a, b))
+print(mull(a, b))
